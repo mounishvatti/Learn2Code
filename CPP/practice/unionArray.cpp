2484e6d2b7edda07faa032c89bd201e65dc71bf5
mounishvatti <mounishvatti2002@gmail.com>
2024-07-07 11:52:43 +0530

    minor updates

--- CPP/practice/unionArray.cpp
+++ CPP/practice/unionArray.cpp
@@ -1,17 +1,20 @@
-#include<iostream>
+#include <iostream>
 #include <set>
 #include <vector>
 
 using namespace std;
 
-vector<int> unionArr(vector<int>&arr){
+vector<int> unionArr(vector<int> &arr)
+{
 	int n = arr.size();
-	set<int>s;
-	for(int i=0; i<n; i++){
+	set<int> s;
+	for (int i = 0; i < n; i++)
+	{
 		s.insert(arr[i]);
 	}
 }
-int main(){
+int main()
+{
 	/*
 		your code goes here
 	*/
@@ -19,41 +22,54 @@ int main(){
 
 // Optimal solution
 
-vector<int> unionArr(vector<int>&arr1, vector<int>&arr2){
+vector<int> unionArr(vector<int> &arr1, vector<int> &arr2)
+{
 	// arr1 = 1,1,2,3,4,5
 	// arr2 = 2,3,4,5,6,7,8
-	
+
 	// unionArray = 1,2,3,4,5,6,7,8
-	
+
 	int n1 = arr1.size();
 	int n2 = arr2.size();
-	vector<int>uA;
+	vector<int> uA;
 	int i = 0;
 	int j = 0;
-	while(i<n1 && j<n2){
-		if(arr1[i]<=arr2[j]){
-			if(uA.size() == 0 || uA.back()!=arr1[i]){
+	while (i < n1 && j < n2)
+	{
+		if (arr1[i] <= arr2[j])
+		{
+			if (uA.size() == 0 || uA.back() != arr1[i])
+			{
 				uA.push_back(arr1[i]);
 			}
 			i++;
-		}else if(arr2[j]<arr1[i]){
-			if(uA.size() == 0 || uA.back()!=arr2[j]){
+		}
+		else if (arr2[j] < arr1[i])
+		{
+			if (uA.size() == 0 || uA.back() != arr2[j])
+			{
 				uA.push_back(arr2[j]);
 			}
 			j++;
 		}
 	}
-	while(i<n1){
-		if(arr1[i]<=arr2[j]){
-			if(uA.size() == 0 || uA.back()!=arr1[i]){
+	while (i < n1)
+	{
+		if (arr1[i] <= arr2[j])
+		{
+			if (uA.size() == 0 || uA.back() != arr1[i])
+			{
 				uA.push_back(arr1[i]);
 			}
 			i++;
 		}
 	}
-	while(j<n2){
-		if(arr2[j]<arr1[i]){
-			if(uA.size() == 0 || uA.back()!=arr2[j]){
+	while (j < n2)
+	{
+		if (arr2[j] < arr1[i])
+		{
+			if (uA.size() == 0 || uA.back() != arr2[j])
+			{
 				uA.push_back(arr2[j]);
 			}
 			j++;
