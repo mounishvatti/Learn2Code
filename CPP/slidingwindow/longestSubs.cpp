8d8ed63b52a5cd940088479576b4a61e55a686a6
mounishvatti <mounishvatti2002@gmail.com>
2024-07-15 06:45:18 +0530

    optimal approach for longest substring without repeating characters

--- CPP/slidingwindow/longestSubs.cpp
+++ CPP/slidingwindow/longestSubs.cpp
@@ -3,7 +3,9 @@
 #include <iostream>
 #include <string>
 #include <unordered_set>
+#include <unordered_map>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 int funcLongestSubstring(string st) // brute force
@@ -30,8 +32,45 @@ int funcLongestSubstring(string st) // brute force
     return maxLen;
 }
 
+// optimal approach
+/*
+    Initially take a hashvector of size 256 (as there are 256 characters present) and initialise all with -1.
+    Then take a window of size 0 and start traversing the string.
+    If the character is not present in the hashvector then add it to the hashvector and increment the window size (right++).
+    If the character is present in the hashvector then increment the window size (left).
+    Then compare the window size with the max window size and update the max window size if required.
+    Then return the max window size.
+*/
+
+int longestSubs(string str)
+{
+    int n = str.length();
+    int l = 0;
+    int r = 0;
+    int maxLen = 0;
+    vector<int> hash(256, -1);
+    while (r < n)
+    {
+        if (hash[str[r]] != -1)
+        { // already present
+            if (hash[str[r]] >= l)
+            {
+                l = hash[str[r]] + 1; // updating the left pointer
+            }
+        }
+        int len = r - l + 1;
+        maxLen = max(len, maxLen);
+        hash[str[r]] = r;
+        r++;
+    }
+    return maxLen;
+}
+
 int main()
 {
     string s = "abcabcbb";
     cout << funcLongestSubstring(s);
+
+    cout << "The length of the longest substring without repeating characters is: " << longestSubs(s) << endl;
+    return 0;
 }
