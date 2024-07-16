8a31d9512e4e261d50e6cbeea1c0efa7ca2e257c
mounishvatti <mounishvatti2002@gmail.com>
2024-07-16 17:44:41 +0530

    readable code

--- CPP/Recursion/fibonacci.cpp
+++ CPP/Recursion/fibonacci.cpp
@@ -1,45 +1,57 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int fibonacci(int n){ // Time complexity: (2^n) exponential, Space complexity O(N)
-    if(n==0 || n==1){
+int fibonacci(int n)
+{ // Time complexity: (2^n) exponential, Space complexity O(N)
+    if (n == 0 || n == 1)
+    {
         return n;
     }
 
-    return fibonacci(n-1) + fibonacci(n-2);
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-int fibonacci_n(int n){ // Time complexity O(N), Space complexity O(1)
-    if(n==0 || n==1){
+int fibonacci_n(int n)
+{ // Time complexity O(N), Space complexity O(1)
+    if (n == 0 || n == 1)
+    {
         return n;
     }
-    int a=0, b=1, c;
-    for(int i=2; i<=n; i++){
-        c=a+b;
-        a=b;
-        b=c;
+
+    int a = 0, b = 1, c;
+    for (int i = 2; i <= n; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
     }
     return c;
 }
 
-int main(){
+int main()
+{
     int n = 10;
 
-    vector<int>fibonacci_series;
+    vector<int> fibonacci_series;
 
     int first = 0;
     int second = 1;
-    for(int i=0; i<=n; i++){
+
+    for (int i = 0; i <= n; i++)
+    {
         fibonacci_series.push_back(first);
         int next = first + second;
         first = second;
         second = next;
     }
-    for (auto it: fibonacci_series){
-        cout<<it<<" ";
+
+    for (auto it : fibonacci_series)
+    {
+        cout << it << " ";
     }
-    cout<<endl;
+
+    cout << endl;
     cout << fibonacci(n) << endl;
     cout << fibonacci_n(n) << endl;
 }
