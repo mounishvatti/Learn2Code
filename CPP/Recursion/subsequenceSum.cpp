bef8a84e838289fa7b7c4877d2010cb111728f48
mounishvatti <mounishvatti2002@gmail.com>
2024-07-17 08:32:58 +0530

    print the count of subsequences

--- CPP/Recursion/subsequenceSum.cpp
+++ CPP/Recursion/subsequenceSum.cpp
@@ -51,15 +51,32 @@ bool subseqSum1(int i, vector<int>& temp, vector<int>& arr, int sum, int s){
     return false;
 }
 
+int subseqSumCount(int i, vector<int>& arr, int sum, int s){
+    if(i == arr.size()){
+        if(s==sum) return 1;    
+        else return 0;
+    }
+
+    s+=arr[i];
+    int l = subseqSumCount(i + 1, arr, sum, s);
+
+    s-=arr[i];
+    int r = subseqSumCount(i + 1, arr, sum, s); 
+
+    return l+r;
+}
+
 int main(){
     vector<int> arr = {1, 2, 1};
     vector<int> temp;
-    
+
     int sum = 2;
 
-    subseqSum(0, temp, arr, sum, 0);
+    subseqSum(0, temp, arr, sum, 0); // print all subsequences
     cout << endl;
-    subseqSum1(0, temp, arr, sum, 0);
+    subseqSum1(0, temp, arr, sum, 0); // print only one subsequence
+    cout << endl;
+    cout << subseqSumCount(0, arr, sum, 0) << endl; // print count
 
     return 0;
 }
