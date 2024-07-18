e8062f97099e3ee31f2de99f214a6300cc7c96bd
mounishvatti <mounishvatti2002@gmail.com>
2024-07-18 11:01:50 +0530

    updated codes

--- CPP/bitmanipulation/set_unset.cpp
+++ CPP/bitmanipulation/set_unset.cpp
@@ -3,13 +3,19 @@ using namespace std;
 
 // check if the ith bit is set or not
 
-bool check(int n, int i){
-    if(n & (1 << i) > 0) return true;
+bool check1(int n, int i){
+    if(n & (1 << i) > 0) return true; // using left shift
     else return false;
+}
+
+bool check2(int n, int i){
+    if((n >> i)& 1 == 0) return false; // using right shift
+    else return true;
 }
 
 int main(){
     int n = 13;
     int i = 2;
-    cout << check(n, i);
+    cout << check1(n, i) << endl;
+    cout << check2(n, i) << endl;
 }
--- python/practice/bitmanipulation/swap.py
+++ python/practice/bitmanipulation/swap.py
@@ -1,11 +1,20 @@
+# Method - 1 (Using a temporary variable)
+a = 10
+b = 15
+
+temp = a
+a = b
+b = temp
+
+# Method - 2 (Using addition and subtraction)
 a = 10
 b = 15
 
 a = a + b # 25
-
 b = a - b # 25-15 = 10
 a = a - b # 25-10 = 15
 
+# Method - 3 (Using XOR operator)
 c = 10
 d = 15
 
@@ -13,6 +22,22 @@ c = c ^ d
 d = c ^ d # (c ^ d) ^ d = c
 c = c ^ d # (c ^ d) ^ c = d
 
+# Method - 4 (Using multiplication and division)
+e = 10
+f = 5
+
+e = e * f
+f = int(e/f) # (e * f)/f = e 
+e = int(e/f) # (e * f)/e = f
+
+# Method - 5 (Using python's tuple unpacking)
+g = 10
+h = 15
+
+g, h = h, g
+
+########################
+
 print(a,b)
 print(c,d)
-
+print(e,f)
